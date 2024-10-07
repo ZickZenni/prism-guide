e1d9f77d33751a9160a4a323ad7e617b47ca0c49
Calvin Rohloff <zickzenni@gmail.com>
2024-10-07 15:11:41 +0200

    fix: loading guides from disk

---
+++ src/lib/guides.ts
@@ -0,0 +1,12 @@
+export const GUIDES = [
+	{
+		path: 'getting-started',
+		name: 'Erste Schritte',
+		file: 'getting-started.md'
+	},
+	{
+		path: 'user-interface',
+		name: 'User Interface',
+		file: 'user-interface.md'
+	}
+];
--- src/routes/[guide]/+page.server.ts
+++ src/routes/[guide]/+page.server.ts
@@ -1,7 +1,8 @@
 import { fileURLToPath } from 'url';
 import type { Load } from '@sveltejs/kit';
 import { dirname, resolve } from 'path';
-import { existsSync, readFileSync } from 'fs';
+import { readFileSync } from 'fs';
+import { GUIDES } from '$lib/guides';
 
 export interface GuideLoadResponse {
 	content: string | null;
@@ -25,20 +26,13 @@ export const load: Load = ({ params }) => {
 	const __dirname = dirname(__filename);
 
 	const guidesPath = resolve(__dirname, '..', '..', 'guides');
-	const configPath = resolve(guidesPath, 'guides.json');
 
-	if (!existsSync(configPath))
-		return {
-			content: null
-		};
-
-	const json = JSON.parse(readFileSync(configPath, 'utf8'));
 	let response: GuideLoadResponse = {
 		content: null,
 		guides: []
 	};
 
-	for (const g of json) {
+	for (const g of GUIDES) {
 		if (g.path === guide) {
 			const content = readFileSync(resolve(guidesPath, g.file), 'utf8');
 			response = {
